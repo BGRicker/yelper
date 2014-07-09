c201cc571cd713988c9f8486954359e56142c147
Ben Ricker <BGRicker@gmail.com>
2014-07-09 22:19:58 +0000

    post humanize ratings

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ActiveRecord::Base
 	 belongs_to :user
 	 belongs_to :place
+
+
+	 RATINGS = {
+	 	'one star' 		=> '1_star',			#user sees value on the left, right stored in database
+	 	'two stars'		=> '2_stars',
+	 	'three stars'	=> '3_stars',
+	 	'four stars'	=> '4_stars',
+	 	'five stars'	=> '5_stars'
+	 }
+
+	 def humanized_rating						#inverts table for RATINGS
+	 	RATINGS.invert[self.rating]				#calling .rating on self
+	 end
 end
